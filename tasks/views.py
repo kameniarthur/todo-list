5ed3b0e8c462572eeea2d5d866bad4152a3a900e
kameniarthur <kameniarthur138@gmail.com>
2026-01-11 21:40:48 +0100

    create authenticating system

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -7,6 +7,9 @@ class TaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     filterset_fields = ['done']
 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Add appropriate permissions if needed
+
 class TaskDetailview(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
--- todolist_project/settings.py
+++ todolist_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'tasks',
     'django_filters',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 # Internationalization
